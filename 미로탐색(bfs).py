2b56fcce0a75a7385f613f73f130a03633400d2c
secrett2633 <secrett2633@kakao.com>
2022-04-08 14:29:38 +0000

    bfs

--- 미로탐색(bfs).py
+++ 미로탐색(bfs).py
@@ -6,39 +6,27 @@ def bfs(x, y):
     queue = deque()
     queue.append((x, y))
     # 큐가 빌 때까지 반복
-    count = 0
     while queue:      
         # 큐에서 하나의 원소를 뽑아 출력
         x, y = queue.popleft()
         for i in range(4):
           nx = x + dx[i]
           ny = y + dy[i]
-          if nx <0 or ny < 0 or nx >= n or ny >= n:
+          if nx <0 or ny < 0 or nx >= n or ny >= m:
             continue
           if graph[nx][ny] == 0:
             continue
-          if graph[nx][ny] == 1 and not visited[nx][ny]:
-            count += 1
-            visited[nx][ny] = True
+          if graph[nx][ny] == 1:
+            graph[nx][ny] = graph[x][y] + 1
             queue.append((nx, ny))
-    return count
+    return graph[n - 1][m - 1]
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
 
-n = int(input())
+n, m = map(int, input().split())
 graph = []
-visited = [[False] * (n+1) for i in range(n+1)]
 for i in range(n):
   graph.append(list(map(int, input())))
 dx = [-1, 1, 0, 0]
 dy = [0, 0 , -1, 1]
 # 정의된 BFS 함수 호출
-list1 = []
-for i in range(n):
-  for j in range(n):
-    a = bfs(i, j)
-    if a != 0:
-      list1.append(a)
-list1.sort()
-print(len(i))
-for i in list1:
-  print(i)
+print(bfs(0, 0))
