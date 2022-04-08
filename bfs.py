d90e95c946fe89dfa432897c8b16a0073747f69b
secrett2633 <secrett2633@kakao.com>
2022-04-08 06:03:16 +0000

    dfs&bfs

--- bfs.py
+++ bfs.py
@@ -18,33 +18,20 @@ def bfs(graph, start, visited):
                 visited[i] = True
 
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
-
-n, m, v = map(int, input().split())
-graph = [[] for i in range(n + 1)]
-for i in range(m):
-  a, b = map(int, input().split())
-  graph[a].append(b)
-  graph[b].append(a)
-for i in range(len(graph)):
-  graph[i].sort()
-# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
-  
-def dfs(graph, v, visited):
-    # 현재 노드를 방문 처리
-    visited[v] = True
-    print(v, end=' ')
-    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+graph = [
+  [],
+  [2, 3, 8],
+  [1, 7],
+  [1, 4, 5],
+  [3, 5],
+  [3, 4],
+  [7],
+  [2, 6, 8],
+  [1, 7]
+]
 
 # 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
-visited = [False] * (n + 1)
-
-# 정의된 DFS 함수 호출
-dfs(graph, v, visited)
-print("")
-visited = [False] * (n + 1)
+visited = [False] * 9
 
 # 정의된 BFS 함수 호출
-bfs(graph, v, visited)
+bfs(graph, 1, visited)
