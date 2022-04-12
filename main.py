ab21499bd4e4263e49323895adbb3bc9cfe71905
secrett2633 <secrett2633@kakao.com>
2022-04-12 14:16:40 +0000

    1234

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import sys
 input = sys.stdin.readline
-n, r, c = map(int, input().split())
-graph = (2 ** n) * (2 ** n) 
-start = 0
-end = graph - 1
-while graph > 4:
-  if r // (graph ** (0.5) // 2) == 0:
-    if c // (graph ** (0.5) // 2) == 0: #1사분면
-      end = (end + 1) // 4 - 1
-    else: #2사분면
-      start += (graph // 4)
-      end = start + (graph // 4) - 1
-  elif r // (graph ** (0.5) // 2) == 1:
-    if c // (graph ** (0.5) // 2) == 0: #3사분면
-      start += (graph // 4 * 2) 
-      end = start + (graph // 4) - 1
-    else: #4사분면
-      start += (graph // 4 * 3) 
-      end = start + (graph // 4) - 1
-  r %= (graph ** (0.5) // 2)
-  c %= (graph ** (0.5) // 2)
-  graph = graph // 4
-print(int(start + r * 2 + c))
+n = int(input())
+m = int(input())
+if m != 0:
+  arr = list(map(int, input().split()))
+else:
+  arr = []
+  
+cnt = abs(n - 100)
+for now in range(1000001):
+  for i in range(len(str(now))):
+    if int(str(now)[i]) in arr:
+      break
+    elif i == len(str(now)) - 1:
+      cnt = min(cnt, abs(now - n) + len(str(now)))
+print(cnt)
