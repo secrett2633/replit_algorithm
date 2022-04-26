e435fd9c4712542c58ce8cba7703854824d59e9c
secrett2633 <secrett2633@kakao.com>
2022-04-26 15:08:40 +0000

    2022.04.27

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import sys
-sys.setrecursionlimit(10**9)
-a, b = map(int, input().split())
-cache = dict()
-def solve(now, cnt):
-  if now in cache:
-    if cache[now] <= cnt:
-      return
-  cache[now] = cnt
-  if now >= b:
-    return  
-  solve(now * 2, cnt + 1)
-  solve(now * 10 + 1, cnt + 1)
+import heapq
+input = sys.stdin.readline
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n + 1)]
+distance = [int(1e9)] * (n + 1)
+visited = [-1] * (n + 1)
+for i in range(m):
+  a, b, c = map(int, input().split())
+  graph[a].append((b, c))
+  
+tx, ty = map(int, input().split())
 
-solve(a, 0)
-print(cache[b] + 1 if b in cache else "-1")
+def dijkstra(start):
+  q = []
+  heapq.heappush(q, (0, start))
+  distance[start] = 0
+  while q:
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+      continue
+    for i in graph[now]:
+      cost = dist + i[1]
+      if cost < distance[i[0]]:
+        distance[i[0]] = cost
+        heapq.heappush(q, (cost, i[0]))
+        visited[i[0]] = now
+
+dijkstra(tx) 
+print(distance[ty])
+
+result = [ty]
+temp = ty
+while visited[temp] != -1:
+    result.append(visited[temp])
+    temp = visited[temp]
+print(len(result))
+for i in result[::-1]:
+  print(i, end=' ')
+print("")
