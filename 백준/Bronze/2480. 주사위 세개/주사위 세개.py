603efd554969e0481031f89c864d35bf95b1607c
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-05-25 20:56:25 +0900

    [Bronze IV] Title: 주사위 세개, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+import math
+a, b, c = map(int, input().split())
+if a == b == c:
+  print(10000 + 1000 * a)
+elif a == b:
+  print(1000 + a * 100)
+elif a == c:
+  print(1000 + a * 100)
+elif c == b:
+  print(1000 + b * 100)
+else:
+  print(max(a, b, c) * 100)
