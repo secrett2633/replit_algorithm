14a3ee7d840205d61fd60daa2edfc73127bd4def
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-05 21:18:20 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.py
@@ -0,0 +1,8 @@
+import sys
+#from math import sqrt
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+n, m = map(int, input().split())
+print(n * m // 2)
