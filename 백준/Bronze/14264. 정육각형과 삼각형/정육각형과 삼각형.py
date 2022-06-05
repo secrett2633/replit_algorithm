cd5d78600fed10a371c3712241deccf3d9dba4e1
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-05 20:49:20 +0900

    [Bronze IV] Title: 정육각형과 삼각형, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.py
@@ -0,0 +1,8 @@
+import sys
+from math import sqrt
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+l = int(input())
+print(l * l * sqrt(3) / 4)
