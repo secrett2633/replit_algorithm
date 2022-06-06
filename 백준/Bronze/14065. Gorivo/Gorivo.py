171b54a829e3e0ed87df02e021edeb0e62b97ec6
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-06 22:11:18 +0900

    [Bronze IV] Title: Gorivo, Time: 104 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/14065. Gorivo/Gorivo.py
@@ -0,0 +1,8 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+x = float(input())
+print(round(100.0 / (x * (1.609344 / 3.785411784)), 6))
