f4ff74a2d7657e05260e024a325d7401c1a842e3
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-06 20:27:34 +0900

    [Bronze IV] Title: FYI, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.py
@@ -0,0 +1,11 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+n = input()
+if n[:3] == "555":
+  print("YES")
+else:
+  print("NO")
