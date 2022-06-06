191ded751a0dbd55425430c5debd7fa2a2c6cec0
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-06 19:35:24 +0900

    [Bronze IV] Title: 자동완성, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.py
@@ -0,0 +1,11 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+s = input().rstrip()
+if s == "N" or s == "n":
+  print("Naver D2")
+else:
+  print("Naver Whale")
