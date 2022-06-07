e35ae2f3212c5bf24df06d61f0bb43936ac92f62
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-07 21:00:16 +0900

    [Bronze IV] Title: Archivist, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,9 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+arr = ['ITMO', 'SPbSU', 'SPbSU', 'ITMO', 'ITMO', 'SPbSU', 'ITMO', 'ITMO', 'ITMO', 'ITMO', 'ITMO', 'PetrSU, ITMO', 'SPbSU', 'SPbSU', 'ITMO', 'ITMO', 'ITMO', 'ITMO', 'SPbSU', 'ITMO', 'ITMO', 'ITMO', 'ITMO', 'SPbSU', 'ITMO']
+n = int(input()) - 1995
+print(arr[n])
