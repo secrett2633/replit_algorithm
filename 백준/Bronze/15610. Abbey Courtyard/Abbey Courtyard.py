47f207e85a1a3de5a00d4bd9faa701653a931553
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-07 22:54:14 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.py
@@ -0,0 +1,8 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+n = int(input())
+print(n ** (0.5) * 4)
