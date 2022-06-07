4c61a6a5d1a8b11d02c4136505ab30f8bf2803e6
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-07 21:07:17 +0900

    [Bronze IV] Title: Atrium, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.py
@@ -0,0 +1,8 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+print(int(input()) ** (0.5) * 4)
+
