2bf973131eddb057a307200d7753605c137d8c85
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-07 22:09:32 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.py
@@ -0,0 +1,8 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+arr = sorted(list(map(int, input().split())))
+print(arr[1])
