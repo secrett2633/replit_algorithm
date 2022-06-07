8466a6cc628c05a4f42963c7a1a9f3d637221417
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-08 00:42:25 +0900

    [Bronze IV] Title: SMS from MCHS, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/21638. SMS from MCHS/SMS from MCHS.py
@@ -0,0 +1,16 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+t1, v1 = map(int, input().split())
+t2, v2 = map(int, input().split())
+if t2 < 0 and v2 >= 10:
+  print("A storm warning for tomorrow! Be careful and stay home if possible!")
+elif t2 < t1:
+  print("MCHS warns! Low temperature is expected tomorrow.")
+elif t2 >= t1 and v2 > v1:
+  print("MCHS warns! Strong wind is expected tomorrow.")
+else:
+  print("No message")
