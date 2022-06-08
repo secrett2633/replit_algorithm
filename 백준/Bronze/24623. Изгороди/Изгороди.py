25f0db20de7ec79de1ad548301ff68cb6f99c9dc
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-09 00:07:47 +0900

    [Bronze IV] Title: Изгороди, Time: 68 ms, Memory: 32952 KB -BaekjoonHub

---
+++ 백준/Bronze/24623. Изгороди/Изгороди.py
@@ -0,0 +1,9 @@
+import sys
+import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+n = int(input())
+a = int(input())
+print(round((180 - a) / 2 + a / 2))
