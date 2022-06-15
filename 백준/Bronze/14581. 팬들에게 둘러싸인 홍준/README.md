f77c136b5a83589ecd2c9e0efc8fcafcdb0831f6
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:44:04 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/README.md
@@ -0,0 +1,12 @@
+# [Bronze IV] 팬들에게 둘러싸인 홍준 - 14581 
+
+[문제 링크](https://www.acmicpc.net/problem/14581) 
+
+### 성능 요약
+
+메모리: 113112 KB, 시간: 112 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.py
@@ -0,0 +1,10 @@
+import sys
+from math import sqrt
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+s = input().rstrip()
+print(":fan::fan::fan:")
+print(":fan::" + s + "::fan:")
+print(":fan::fan::fan:")
