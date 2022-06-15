d75c51b301674bff8918553730d9e82b4dbb6c0a
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 08:12:34 +0900

    [Bronze IV] Title: SciComLove, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.py
@@ -0,0 +1,9 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+n = int(input())
+for i in range(n):
+  print("SciComLove")
