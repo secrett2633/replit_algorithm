db905fc383515f272bb756749dd5dfc3e942bae5
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:12:23 +0900

    [Bronze III] Title: ACM 호텔, Time: 116 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):  
+    h, w, n = map(int, input().split())
+    num = n // h + 1
+    floor = n % h
+    if n % h == 0:
+        num = n // h
+        floor = h
+    print(str(floor * 100 + num))
+      
