0f532d38ebdc3484967ae36a489b78933ce325a7
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:55:22 +0900

    [Bronze IV] Title: 수찬은 마린보이야!!, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/15921. 수찬은 마린보이야！！/수찬은 마린보이야！！.py
@@ -0,0 +1,12 @@
+import sys
+import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().rstrip().split()))
+if n == 0:
+  print("divide by zero")
+else:
+  print("1.00")
