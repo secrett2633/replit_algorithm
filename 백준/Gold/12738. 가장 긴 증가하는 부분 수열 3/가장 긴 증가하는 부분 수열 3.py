0e74f5ae8528b78123703e97466130d84aa43bf7
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:34:38 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 552 ms, Memory: 259684 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+distance = [-1000000001]
+
+for a in arr:
+    if distance[-1] < a:
+        distance.append(a)
+    else:
+        left = 0
+        right = len(distance)
+
+        while left < right:
+            mid = (right + left) // 2
+            if distance[mid] < a:
+                left = mid + 1
+            else:
+                right = mid
+        distance[right] = a
+
+print(len(distance) - 1)
