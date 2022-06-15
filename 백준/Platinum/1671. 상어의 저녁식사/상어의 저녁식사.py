06f0af6182553d5adfd65b91f15d7816d5b9b68f
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 05:58:20 +0900

    [Platinum III] Title: 상어의 저녁식사, Time: 136 ms, Memory: 117248 KB -BaekjoonHub

---
+++ 백준/Platinum/1671. 상어의 저녁식사/상어의 저녁식사.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(x):
+    if visited[x]:
+        return False    
+    visited[x] = 1    
+    for i in range(1, n+1):
+        if check[x][i]:
+            if not eat[i] or dfs(eat[i]):
+                eat[i] = x
+                return True
+    return False    
+
+n = int(input())
+arr = [[]] + [list(map(int, input().split())) for _ in range(n)]
+check = [[0] * (n + 1) for _ in range(n + 1)]
+eat = [0] * (n + 1) 
+    
+for i in range(1, n + 1):
+    for j in range(1, n + 1):         
+        if i == j or (arr[i][0] == arr[j][0] and arr[i][1] == arr[j][1] and arr[i][2] == arr[j][2] and i > j):
+            continue            
+        elif arr[i][0] >= arr[j][0] and arr[i][1] >= arr[j][1] and arr[i][2] >= arr[j][2]:
+            check[i][j] = 1   
+
+for i in range(2):
+    for j in range(1, n + 1):
+        visited = [0] * (n + 1)
+        dfs(j) 
+
+print(eat[1:].count(0))
