84d63042ba9c8cfbaa8b66fd98210fed668c1bad
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:59:07 +0900

    [Bronze IV] Title: Contemporary Art, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/16600. Contemporary Art/Contemporary Art.py
@@ -0,0 +1,8 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+a = int(input())
+print(a ** (0.5) * 4)
---
+++ 백준/Bronze/16600. Contemporary Art/README.md
@@ -0,0 +1,12 @@
+# [Bronze IV] Contemporary Art - 16600 
+
+[문제 링크](https://www.acmicpc.net/problem/16600) 
+
+### 성능 요약
+
+메모리: 113112 KB, 시간: 124 ms
+
+### 분류
+
+기하학(geometry), 수학(math)
+
