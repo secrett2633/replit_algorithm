b88117827479504058e03a51692113d312b181b9
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:03:37 +0900

    [Platinum II] Title: 배열의 힘, Time: 1668 ms, Memory: 153612 KB -BaekjoonHub

---
+++ 백준/Platinum/8462. 배열의 힘/README.md
@@ -0,0 +1,12 @@
+# [Platinum II] 배열의 힘 - 8462 
+
+[문제 링크](https://www.acmicpc.net/problem/8462) 
+
+### 성능 요약
+
+메모리: 153612 KB, 시간: 1668 ms
+
+### 분류
+
+mo's(mo), 오프라인 쿼리(offline_queries)
+
---
+++ 백준/Platinum/8462. 배열의 힘/배열의 힘.py
@@ -0,0 +1,55 @@
+import sys
+from functools import cmp_to_key
+from math import sqrt
+input = sys.stdin.readline
+def querysort(q1, q2):
+    if q1[1] // sqrtN != q2[1] // sqrtN:
+        if q1[1] // sqrtN < q2[1] // sqrtN:
+            return 1
+        else:
+            return -1
+    else:
+        if q1[2] < q2[2]:
+            return 1
+        else:
+            return -1
+def Plus(x):
+    global res
+    count[x] += 1
+    res += (count[x] * 2 - 1) * x
+def Minus(x):
+    global res
+    count[x] -= 1
+    res -= (count[x] * 2 + 1) * x
+
+n, t = map(int, input().split())
+V = [0] + list(map(int, input().split()))
+sqrtN = sqrt(n)
+
+count = [0] * 1000002
+res = 0
+query = []
+result = [0] * (t)
+for cnt in range(t):
+    i, j = map(int, input().split())
+    query.append((cnt, i, j))
+query = sorted(query, key=cmp_to_key(querysort))
+s = 0
+e = 0
+res = 0
+for i in range(t):
+    while s < query[i][1]:
+        Minus(V[s])
+        s += 1
+    while s > query[i][1]:
+        s -= 1
+        Plus(V[s])
+    while e < query[i][2]:
+        e += 1
+        Plus(V[e])
+    while e > query[i][2]:
+        Minus(V[e])
+        e -= 1
+    result[query[i][0]] = res
+for i in range(t):
+    print(result[i])
