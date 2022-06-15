995f548c42b36e257f35fe35c34e19df12ca6903
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:58:52 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.py
@@ -0,0 +1,9 @@
+import sys
+import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+d1 = int(input())
+d2 = int(input())
+print(d2 * 2 * 3.141592 + 2 * d1)
