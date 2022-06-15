1df71965ec6e632e273c5346557024ebb55958f1
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 08:19:21 +0900

    [Bronze IV] Title: 녹색거탑, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.py
@@ -0,0 +1,11 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+s = int(input())
+res = 1
+for i in range(s):
+  res *= 2
+print(res)
