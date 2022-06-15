5ff465d1c57c7a0cf565f43446048561aae8357d
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:40:34 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 160 ms, Memory: 127340 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/README.md
@@ -0,0 +1,12 @@
+# [Gold IV] 숨바꼭질 4 - 13913 
+
+[문제 링크](https://www.acmicpc.net/problem/13913) 
+
+### 성능 요약
+
+메모리: 127340 KB, 시간: 160 ms
+
+### 분류
+
+너비 우선 탐색(bfs), 그래프 이론(graphs), 그래프 탐색(graph_traversal)
+
---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -0,0 +1,24 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+graph = [0] * 100001
+visited = [0] * 100001
+queue = deque()
+queue.append(n)
+while queue:
+    v = queue.popleft()        
+    if v == k:
+      print(graph[v])
+      arr = []
+      temp = v
+      for _ in range(graph[v] + 1):
+          arr.append(temp)
+          temp = visited[temp]
+      print(' '.join(map(str, arr[::-1])))
+      break
+    for i in (v - 1, v + 1, v * 2):
+      if 0 <= i <= (100000) and graph[i] == 0:
+        queue.append(i)
+        graph[i] = graph[v] + 1
+        visited[i] = v
