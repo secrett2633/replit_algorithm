5be9a1ba46336a04aab9a13eddc4a388bb386daa
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:01:06 +0900

    [Bronze IV] Title: ISBN, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/6810. ISBN/ISBN.py
@@ -0,0 +1,12 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+a = int(input())
+b = int(input())
+c = int(input())
+res = a + b * 3 + c
+print("The 1-3-sum is " + str(91 + res))
+  
