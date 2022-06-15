e5f0214bfa7c3ab650ed7d73be0f706bd130bb8c
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 06:55:35 +0900

    [Bronze V] Title: 콜센터, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+print("     /~\\")
+print("    ( oo|")
+print("    _\\=/_")
+print("   /  _  \\")
+print("  //|/.\\|\\\\")
+print(" ||  \\ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
