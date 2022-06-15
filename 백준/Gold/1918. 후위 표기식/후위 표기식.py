cd7c3aff8a52f43bc936a03833abab5df054a922
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 06:03:33 +0900

    [Gold II] Title: 후위 표기식, Time: 132 ms, Memory: 114276 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+arr = list(input().rstrip())
+q = deque(arr)
+result = ""
+oper = deque()
+words = ""
+bracket = 0
+while q:
+  a = q.popleft()
+  if a == "+" or a == "-":
+    while oper and oper[-1] != "(":
+      result += oper.pop()
+    oper.append(a)
+  elif a == "*" or a == "/":
+    while oper and (oper[-1] == "*" or oper[-1] == "/"):
+      result += oper.pop()
+    oper.append(a)
+  elif a == "(":
+    oper.append(a)
+  elif a == ")":
+    while oper and oper[-1] != "(":
+      result += oper.pop()
+    oper.pop()
+  else:
+    result += a
+while oper:
+  result += oper.pop()
+print(result)
