05756f03977b79670d4bdaa6cb184358bb3fd872
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:35:23 +0900

    [Silver I] Title: 1로 만들기 2, Time: 1252 ms, Memory: 523260 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+result = [[0, []] for _ in range(N + 1)]
+result[1][0] = 0 
+result[1][1] = [1] 
+
+for i in range(2, N + 1):	
+    result[i][0] = result[i-1][0] + 1
+    result[i][1] = result[i-1][1] + [i]
+    
+    if i % 3 == 0 and result[i//3][0] + 1 < result[i][0]:
+        result[i][0] = result[i//3][0] + 1
+        result[i][1] = result[i//3][1] + [i]
+
+    if i % 2 == 0 and result[i//2][0] + 1 < result[i][0]:
+        result[i][0] = result[i//2][0] + 1
+        result[i][1] = result[i//2][1] + [i]
+        
+print(result[N][0])
+print(" ".join(map(str, reversed(result[N][1]))))
