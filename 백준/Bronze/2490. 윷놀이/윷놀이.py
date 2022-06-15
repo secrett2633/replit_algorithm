28ce21b1066c3bf3f246c0fd5a42d7dae3e9806d
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 06:39:07 +0900

    [Bronze III] Title: 윷놀이, Time: 56 ms, Memory: 29284 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -0,0 +1,13 @@
+for i in range(3):
+      temp = input().split()
+      a = temp.count("0")
+      if a == 1:
+            print("A")
+      elif a == 2:
+            print("B")
+      elif a == 3:
+            print("C")
+      elif a == 4:
+            print("D")
+      else:
+            print("E")
