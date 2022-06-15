03836fdcc02a6dbfa4a202fb71db581e5c68b30d
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:29:22 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 240 ms, Memory: 129592 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr = [0] + arr
+partial_sum = [0] * len(arr)
+for i in range(1, len(arr)):
+    partial_sum[i] = partial_sum[i-1] + arr[i]
+
+for a in range(m):
+    i, j = map(int, input().split())
+    print(partial_sum[j] - partial_sum[i - 1])
