81cab527247cc9bc5d97f94f353b538812d6ae9d
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 05:52:54 +0900

    [Silver V] Title: 소수, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/1312. 소수/README.md
@@ -0,0 +1,12 @@
+# [Silver V] 소수 - 1312 
+
+[문제 링크](https://www.acmicpc.net/problem/1312) 
+
+### 성능 요약
+
+메모리: 114328 KB, 시간: 116 ms
+
+### 분류
+
+수학(math)
+
---
+++ 백준/Silver/1312. 소수/소수.py
@@ -0,0 +1,7 @@
+import sys
+input = sys.stdin.readline
+a, b, n = map(int, input().split())
+a %= b
+for i in range(n - 1):
+    a = (a * 10) % b
+print((a * 10) // b)
