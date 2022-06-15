642a198e52c026eac1e68a2318403a6cce537dd4
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-16 07:56:06 +0900

    [Bronze IV] Title: CASIO, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.py
@@ -0,0 +1,9 @@
+import sys
+#import math
+#from collections import Counter
+#from string import ascii_lowercase
+#alphabet_list = ascii_lowercase
+input = sys.stdin.readline
+a, b = map(int, input().split())
+if a == b: print(1)
+else: print(0)
