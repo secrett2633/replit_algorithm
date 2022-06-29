4705d12b592f38b7c712cf94b2973ee2d4713ec5
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-06-29 12:04:50 +0900

    [Silver V] Title: 투명, Time: 124 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/1531. 투명/투명.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = [[0] * 100 for _ in range(100)]
+for _ in range(n):
+  x1, y1, x2, y2 = map(int, input().split())
+  for i in range(x1 - 1, x2):
+    for j in range(y1 - 1, y2):
+      arr[i][j] += 1
+cnt = 0
+for i in range(100):
+  for j in range(100):
+    if arr[i][j] > m:
+      cnt += 1
+print(cnt)
