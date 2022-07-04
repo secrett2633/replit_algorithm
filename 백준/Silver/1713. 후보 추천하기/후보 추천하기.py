4209a3f4da75f980fd19773024a45f073a48a517
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-07-04 17:54:15 +0900

    [Silver II] Title: 후보 추천하기, Time: 128 ms, Memory: 115268 KB -BaekjoonHub

---
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -0,0 +1,25 @@
+import sys
+#import math
+input = sys.stdin.readline
+n = int(input())
+m = int(input())
+arr = list(map(int, input().split()))
+stu = [0] * 101
+q = []
+for i in arr:
+  stu[i] += 1
+  if i in q: continue
+  if len(q) == n:
+    cnt = 1001
+    for k in range(n):
+      cnt = min(cnt, stu[q[k]])
+    for k in range(n):
+      if stu[q[k]] == cnt:
+        stu[q[k]] = 0
+        del q[k] 
+        cnt = 1001
+        for j in q:
+          cnt = min(cnt, stu[j])
+        break
+  q.append(i)
+print(*sorted(q))
