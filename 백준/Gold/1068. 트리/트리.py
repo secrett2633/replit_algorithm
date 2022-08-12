e86988bc07e3d1ffdec45b44599f9aa4799d85b8
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-08-12 21:00:35 +0900

    [Gold V] Title: 트리, Time: 140 ms, Memory: 114276 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -7,22 +7,16 @@ d = int(input())
 graph = {i:[] for i in range(n)}
 for i in range(n):
   if arr[i] != -1:
-    if i != d:
-      graph[arr[i]].append(i)
-  else:
-    start = i
-visited = [False] * n
+    if i != d: graph[arr[i]].append(i)
+  else: start = i
 if start == d: print(0)
 else:
   cnt = 0
   q = deque([start])
   while q:
     x = q.pop()    
-    if visited[x]: continue
-    visited[x] = True
-    
     if graph[x]:
       for i in graph[x]:
-        if i != d: q.append(i)
-    elif x != d: cnt += 1
+        q.append(i)
+    else: cnt += 1
   print(cnt)
