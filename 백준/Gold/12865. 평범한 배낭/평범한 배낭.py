192eec5366413ef8e57dd212ea0f73a9267ab244
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-08-13 21:59:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 348 ms, Memory: 199608 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,17 +2,15 @@ import sys
 input = sys.stdin.readline
 n, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-arr.insert(0, [0, 0])
-visited = [[0] * (k + 1) for _ in range(n + 1)]
-
-for i in range(1, n + 1):
-    for j in range(1, k + 1):
-        w = arr[i][0]
-        v = arr[i][1]
-
-        if j < w:
-            visited[i][j] = visited[i-1][j]
-        else:
-            visited[i][j] = max(visited[i-1][j], visited[i-1][j-w]+v)
-
-print(visited[n][k])
+dp = [0] * (k + 1)
+for i in range(n):
+  tmp = []
+  for j in range(1, k):
+    if dp[j] != 0 and j + arr[i][0] < k + 1:
+      tmp.append([j, dp[j]])     
+  for a, b in tmp:
+    dp[a + arr[i][0]] = max(b + arr[i][1], dp[a + arr[i][0]])
+  if arr[i][0] < k + 1:
+    dp[arr[i][0]] = max(dp[arr[i][0]], arr[i][1])
+print(max(dp))
+  
