42d1051214119e4c51be3ec852e9d7002307331a
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-08-21 20:47:39 +0900

    [Gold I] Title: 음주 코딩, Time: 1716 ms, Memory: 192476 KB -BaekjoonHub

---
+++ 백준/Gold/5676. 음주 코딩/음주 코딩.py
@@ -0,0 +1,47 @@
+import sys
+input = sys.stdin.readline
+
+def init(node, node1, node2):
+  if node1 == node2:
+    tree[node] = arr[node1]
+  else:
+    tree[node] = init(node * 2, node1, (node1 + node2) // 2) * init(node * 2 + 1, (node1 + node2) // 2 + 1, node2)
+  return tree[node]
+
+def seg_find(node, node1, node2, start, end):
+  if start > node2 or end < node1: return 1
+  elif start <= node1 and end >= node2: return tree[node]
+  else: return seg_find(node * 2, node1, (node1 + node2) // 2, start, end) * seg_find(node * 2 + 1, (node1 + node2) // 2 + 1, node2, start, end)
+
+def seg_change(node, node1, node2, idx, diff):
+  if idx > node2 or idx < node1: return
+  elif node1 == node2: tree[node] = diff
+  else:
+    seg_change(node * 2, node1, (node1 + node2) // 2, idx, diff)
+    seg_change(node * 2 + 1, (node1 + node2) // 2 + 1, node2, idx, diff)
+    tree[node] = tree[node * 2] * tree[node * 2 + 1]
+
+flag = {1 : "+", -1 : "-", 0 : "0"}
+while True:
+  try:
+    n, m = map(int, input().split())
+  except:
+    break
+  arr = list(map(int, input().split()))
+  for i in range(n):
+    if arr[i] > 0: arr[i] = 1
+    elif arr[i] < 0: arr[i] = -1
+  tree = [0] * (4 * n)
+  init(1, 0, n - 1)
+  ans = ""
+  for _ in range(m):
+    a, x, y = input().split()
+    x, y = int(x), int(y)
+    if a == "P":
+      ans += flag[seg_find(1, 0, n - 1, x - 1, y - 1)]
+    else:
+      if y > 0: y = 1
+      elif y < 0: y = -1
+      seg_change(1, 0, n - 1, x - 1, y)
+      arr[x - 1] = y
+  print(ans)
