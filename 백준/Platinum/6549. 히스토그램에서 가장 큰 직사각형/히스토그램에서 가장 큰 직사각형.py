dc3ab54795d694c9d86c18f090f8309da0f38c24
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-08-21 19:07:21 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 356 ms, Memory: 144988 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+def seg_find(start, end):
+  if start == end:
+    return arr[start]
+  else:
+    ans = max(seg_find(start, (start + end) // 2), seg_find((start + end) // 2 + 1, end))
+    i = (start + end) // 2
+    j = i + 1
+    l = min(arr[i], arr[j])
+    s = l * 2    
+    while start < i or end > j:
+      if j == end or i > start and arr[i - 1] >= arr[j + 1]:
+        i -= 1
+        l = min(arr[i], l)
+        s = max(s, l * (j - i + 1))
+      else:
+        j += 1
+        l = min(arr[j], l)
+        s = max(s, l * (j - i + 1))        
+    return max(ans, s)
+while True:
+  n, *arr = list(map(int, input().split()))
+  if n == 0: break
+  print(seg_find(0, n - 1))
