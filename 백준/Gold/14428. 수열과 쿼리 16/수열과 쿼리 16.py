45369fb6a57c4bb7403761e229fe4d6e7374d974
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-08-25 02:31:27 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 1784 ms, Memory: 165068 KB -BaekjoonHub

---
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.py
@@ -0,0 +1,39 @@
+import sys
+import heapq
+input = sys.stdin.readline
+def init(node, node1, node2):
+    if node1 == node2:
+        tree[node] = [arr[node1], node1]
+    else:
+        init(node * 2, node1, (node1 + node2) // 2)
+        init(node * 2 + 1, (node1 + node2) // 2 + 1, node2)
+        tree[node] = sorted([tree[node * 2]] + [tree[node * 2 + 1]])[0]
+def seg_change(node, node1, node2, idx, diff):
+    if idx > node2 or idx < node1: return []
+    elif node1 == node2:
+        tree[node] = [diff, node1]
+    else:        
+        seg_change(node * 2, node1, (node1 + node2) // 2, idx, diff)
+        seg_change(node * 2 + 1, (node1 + node2) // 2 + 1, node2, idx, diff)
+        tree[node] = sorted([tree[node * 2]] + [tree[node * 2 + 1]])[0]
+def seg_count(node, node1, node2, start, end):
+    if end < node1 or start > node2: return []
+    elif start <= node1 and end >= node2: return tree[node]
+    else: 
+        a = seg_count(node * 2, node1, (node1 + node2) // 2, start, end)
+        b = seg_count(node * 2 + 1, (node1 + node2) // 2 + 1, node2, start, end)
+        c = []
+        if a: c += [a]
+        if b: c += [b]
+        return sorted(c)[0]
+n = int(input())
+arr = list(map(int, input().split()))
+m = int(input())
+tree = [[] for _ in range(n * 4)]
+init(1, 0, n - 1)
+for _ in range(m):
+    a, b, c = list(map(int, input().split()))
+    if a == 2: #가장 작은 값의 인덱스 출력
+        print(seg_count(1, 0, n - 1, b - 1, c - 1)[1] + 1)
+    else: #b-1를 c로 바꾸기 
+        seg_change(1, 0, n - 1, b - 1, c)
