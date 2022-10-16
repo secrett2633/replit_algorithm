29292b5b74cc751d62380f4c5c282fd5730a2d4f
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-10-17 03:56:27 +0900

    [Gold V] Title: 줄어드는 수, Time: 140 ms, Memory: 113764 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
@@ -0,0 +1,13 @@
+from collections import deque
+n = int(input())
+visited = []
+q = deque(list(range(10)))
+while q:
+    a = q.pop()
+    visited.append(a)
+    a = str(a)
+    for i in range(int(a[-1])):
+        q.append(int(a + str(i)))
+visited.sort()
+try: print(visited[n - 1])
+except: print(-1)
