8b50f4a869c99d0e3ebad41e90c8bc3979865854
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-12-02 03:57:29 +0900

    [Bronze IV] Title: HOMWRK, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/18398. HOMWRK/HOMWRK.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    for _ in range(n):
+        a, b = map(int, input().split())
+        print(a + b, a * b)
