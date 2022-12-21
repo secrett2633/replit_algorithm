ddb20bb298881554f8d0e547c95e9357592416e3
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2022-12-22 03:40:56 +0900

    [Bronze III] Title: TGN, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    if a == b - c: print("does not matter")
+    elif a > b - c: print("do not advertise")
+    else: print("advertise")
