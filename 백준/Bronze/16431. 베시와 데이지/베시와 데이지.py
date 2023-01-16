9cd0de080917e0a4509fa52a6452c6d3fbb2f9b0
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-01-17 06:00:02 +0900

    [Bronze III] Title: 베시와 데이지, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/16431. 베시와 데이지/베시와 데이지.py
@@ -0,0 +1,8 @@
+b1, b2 = map(int, input().split())
+d1, d2 = map(int, input().split())
+j1, j2 = map(int, input().split())
+a = max(abs(j1 - b1), abs(j2 - b2))
+b = abs(j1 - d1) + abs(j2 - d2)
+if a > b: print("daisy")
+elif a == b: print("tie")
+else: print("bessie")
