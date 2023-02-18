59fe7f17be5052668bb29d0294b51ac1eb2f88a5
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-02-18 10:35:00 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 0.77 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
+++ 프로그래머스/lv3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.py
@@ -3,10 +3,9 @@ def solution(key, lock):
     for i in range(len(lock)):
         for j in range(len(lock[0])):
             cnt += 1 - lock[i][j]
-    if not cnt: return True
     for _ in range(4):
-        for i in range(-len(key) + 1, len(key) + len(lock) + 1):
-            for j in range(-len(key[0]) + 1, len(key[0]) + len(lock[0]) + 1):
+        for i in range(-len(key), len(key) + len(lock)):
+            for j in range(-len(key[0]), len(key[0]) + len(lock[0])):
                 cnt1 = 0
                 flag = False
                 for x in range(len(key)):
