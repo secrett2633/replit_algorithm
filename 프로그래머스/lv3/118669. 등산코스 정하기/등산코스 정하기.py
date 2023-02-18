afb1a4ed87acd5a33e6a01069ef5a64653393e82
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-02-19 02:32:46 +0900

    [level 3] Title: 등산코스 정하기, Time: 684.69 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -0,0 +1,32 @@
+def solution(n, paths, gates, summits):
+    from collections import deque
+    answer = [int(1e9), int(1e9)]
+    graph = {i : [] for i in range(1, n + 1)}
+    for i, j, w in paths:
+        graph[i].append([j, w])
+        graph[j].append([i, w])
+    summit = [False] * (n + 1)
+    for i in summits:
+        summit[i] = True
+    gate = [int(1e9)] * (n + 1)
+    for i in gates:
+        gate[i] = -1
+    q = deque()
+    for start in set(gates):
+        q.append([start, 0])
+    visited = [int(1e9)] * (n + 1)
+    while q:
+        now, cnt = q.popleft()
+        if summit[now]: 
+            if answer[1] > cnt: 
+                answer = [now, cnt]
+            elif answer[1] == cnt: 
+                answer[0] = min(answer[0], now)
+            continue
+        elif visited[now] <= cnt: continue
+        elif cnt > answer[1]: continue
+        # elif cnt == answer[1] and now >= answer[0]: continue #and not gate[now]
+        visited[now] = cnt
+        for i, c in graph[now]:
+            if visited[i] > max(cnt, c): q.append([i, max(cnt, c)])
+    return answer
