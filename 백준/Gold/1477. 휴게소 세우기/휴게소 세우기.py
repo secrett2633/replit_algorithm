71996de2bca02eaefbc92703f98d3599ef616965
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-03-25 16:37:13 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
@@ -2,17 +2,13 @@ import sys
 input = sys.stdin.readline
 n, m, l = map(int, input().split())
 arr = sorted(list(map(int, input().split()))) + [l]
-start, end, res = 1, arr[-1], arr[-1]
+start, end = 1, arr[-1]
 while start <= end:
-    cnt = 0
+    cnt = now = 0
     mid = (start + end) // 2
-    now = 0
     for i in arr:
-        if now + mid < i: 
-            cnt += (i - now - 1) // mid 
+        if now + mid < i: cnt += (i - now - 1) // mid 
         now = i
-    if cnt <= m: 
-        res = min(res, mid)
-        end = mid - 1
+    if cnt <= m: end = mid - 1
     else: start = mid + 1
-print(res)
+print(start)   
