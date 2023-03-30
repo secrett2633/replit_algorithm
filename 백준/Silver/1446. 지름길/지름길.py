f618619b2efbdd183fe40c1d953f1f26fae79116
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-03-31 00:09:37 +0900

    [Silver I] Title: 지름길, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n, d = map(int, input().split())
+graph = sorted([list(map(int, input().split())) for _ in range(n)])
+arr = list(range(d+1))
+for i in range(n):
+    try: 
+        arr[graph[i][1]] = min(arr[graph[i][1]], arr[graph[i][0]] + graph[i][2])
+        for j in range(graph[i][1] + 1, d + 1):
+            arr[j] = min(arr[j], arr[j - 1] + 1)
+    except: continue
+print(arr[d])
