7a2e6761866f132c4597332def072f258904a605
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-03-31 00:21:05 +0900

    [Silver I] Title: 지름길, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -4,9 +4,8 @@ n, d = map(int, input().split())
 graph = sorted([list(map(int, input().split())) for _ in range(n)])
 arr = list(range(d+1))
 for i in range(n):
-    try: 
-        arr[graph[i][1]] = min(arr[graph[i][1]], arr[graph[i][0]] + graph[i][2])
-        for j in range(graph[i][1] + 1, d + 1):
-            arr[j] = min(arr[j], arr[j - 1] + 1)
-    except: continue
+    if graph[i][1] > d: continue
+    arr[graph[i][1]] = min(arr[graph[i][1]], arr[graph[i][0]] + graph[i][2])
+    for j in range(graph[i][1] + 1, d + 1):
+        arr[j] = min(arr[j], arr[j - 1] + 1)
 print(arr[d])
