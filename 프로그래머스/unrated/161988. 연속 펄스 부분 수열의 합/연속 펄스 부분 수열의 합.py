5afa33fe541fa241e96fd7df1e2152787fdb7141
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-04-06 01:37:30 +0900

    [unrated] Title: 연속 펄스 부분 수열의 합, Time: 154.68 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/unrated/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,20 +1,11 @@
 def solution(sequence):
     answer = -int(1e9)
     flag = {0 : 1, 1 : -1}
-    flag1 = [flag[i % 2] for i in range(len(sequence))]
-    flag2 = [flag[(i + 1) % 2] for i in range(len(sequence))]
-    dp1 = [0] * (len(sequence) + 1)
-    dp2 = [0] * (len(sequence) + 1)
+    dp = [0] * (len(sequence) + 1)
     for i in range(len(sequence)):
-        dp1[i + 1] = dp1[i] + sequence[i] * flag1[i]
-        dp2[i + 1] = dp2[i] + sequence[i] * flag2[i]
-    cnt1 = [0,0]
-    cnt2 = [0,0]
+        dp[i + 1] = dp[i] + sequence[i] * flag[i % 2]
+    cnt = [0, 0]
     for i in range(len(sequence), -1, -1):
-        if cnt1[0] < dp1[i]: cnt1[0] = dp1[i]
-        elif cnt1[1] > dp1[i]: cnt1[1] = dp1[i]
-        if cnt2[0] < dp2[i]: cnt2[0] = dp2[i]
-        elif cnt2[1] > dp2[i]: cnt2[1] = dp2[i]
-    # if cnt1[0] < 0 and cnt2[0] < 0: return max(cnt1[0], cnt2[0])
-    # return dp1, cnt1
-    return max(cnt1[0] - cnt1[1], cnt2[0] - cnt2[0])
+        if cnt[0] < dp[i]: cnt[0] = dp[i]
+        elif cnt[1] > dp[i]: cnt[1] = dp[i]
+    return max(cnt[0], cnt[1]) - min(cnt[0], cnt[1])
