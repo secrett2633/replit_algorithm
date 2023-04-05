b2c2ebbcc06ac0101cc59d49be8fd9f8129671a6
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-04-05 09:59:11 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 384.22 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -2,10 +2,12 @@ def solution(board):
     answer = 0
     dp = [[0] * len(board[0]) for _ in range(len(board))]    
     dp[0] = board[0]
-    for x in range(len(board)): dp[x][0] = board[x][0]
-    for x in range(len(dp)):
-        for y in range(len(dp[0])):
-            if x > 0 and y > 0 and board[x][y] == 1:            	
+    for x in range(len(board)): 
+        dp[x][0] = board[x][0]
+    for x in range(1, len(dp)):
+        for y in range(1, len(dp[0])):
+            if board[x][y] == 1:            	
                 dp[x][y] = min(dp[x][y - 1], dp[x - 1][y], dp[x - 1][y - 1]) + 1
-    for i in range(len(dp)): answer = max(answer, max(dp[i]))
+    for i in range(len(dp)): 
+        answer = max(answer, max(dp[i]))
     return answer * answer
