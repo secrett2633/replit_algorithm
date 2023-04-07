b34212e0b275f1741e325032c0c4f1dc9c31bdb7
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-04-07 15:02:52 +0900

    [unrated] Title: 숫자 변환하기, Time: 505.30 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,13 +1,10 @@
 def solution(x, y, n):
-    from collections import deque
-    q = deque([[0, x]])
-    visited = [0] * (y + 1)
-    while q:
-        cnt, now = q.popleft()
-        if visited[now]: continue
-        visited[now] = 1
-        if now == y: return cnt
-        if now + n <= y: q.append([cnt + 1, now + n])
-        if now * 2 <= y: q.append([cnt + 1, now * 2])
-        if now * 3 <= y: q.append([cnt + 1, now * 3])
-    return -1
+    dp = [int(1e9)] * (y + 1)
+    dp[x] = 0
+    
+    for i in range(x, y + 1):
+        if i >= n: dp[i] = min(dp[i], dp[i - n] + 1)
+        if i % 2 == 0: dp[i] = min(dp[i], dp[i // 2] + 1)
+        if i % 3 == 0: dp[i] = min(dp[i], dp[i // 3] + 1)
+        
+    return dp[-1] if dp[-1] != int(1e9) else -1
