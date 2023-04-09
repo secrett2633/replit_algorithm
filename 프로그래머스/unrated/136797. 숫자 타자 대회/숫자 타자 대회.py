f0b0cc3e8b966bf83ab270e338fa69a248e11ac9
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-04-10 00:41:08 +0900

    [unrated] Title: 숫자 타자 대회, Time: 6313.55 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/136797. 숫자 타자 대회/숫자 타자 대회.py
+++ 프로그래머스/unrated/136797. 숫자 타자 대회/숫자 타자 대회.py
@@ -1,19 +1,21 @@
 def solution(numbers):
-    answer = 0
+    def solve(x1, y1, target):
+        a = abs(x1 - target[0])
+        b = abs(y1 - target[1])
+        return max(1, min(a, b) * 3 + (max(a, b) - min(a, b)) * 2)
+    import heapq
+    q = [[0, 1, 0, 1, 2]]
     arr = {"1" : [0, 0], "2" : [0, 1], "3" : [0, 2], "4" : [1, 0], "5" : [1, 1], "6" : [1, 2], "7" : [2, 0], "8" : [2, 1], "9" : [2, 2], "0" : [3, 1]}
     left, right = [1, 0], [1, 2]
-    
-    def solve(now, target):
-        a = abs(now[0] - target[0])
-        b = abs(now[1] - target[1])
-        return max(1, min(a, b) * 3 + (max(a, b) - min(a, b)) * 2)
-    
-    dp = [[[0, 1, 0, 1, 2], [0, 1, 0, 1, 2]]] + [[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0]] for _ in range(len(numbers))]
-    for i in range(len(numbers)):
-        tmp = solve(dp[i][0][1:3], arr[numbers[i]])
-        tmp1 = solve(dp[i][1][1:3], arr[numbers[i]])
-        # if dp[i][0][0] + tmp 
-        # dp[i + 1][1]
-        print(tmp, tmp1)
-        
-    return answer
+    for target in numbers:
+        tmp = []
+        visited = [[[[0] * 3 for _ in range(4)] for _ in range(3)] for _ in range(4)]
+        while q:
+            cnt, x1, y1, x2, y2 = heapq.heappop(q)
+            if (x1, y1) == (x2, y2): continue
+            if visited[x1][y1][x2][y2]: continue
+            visited[x1][y1][x2][y2] = 1
+            heapq.heappush(tmp, [cnt + solve(x1, y1, arr[target])] + arr[target] + [x2, y2])
+            heapq.heappush(tmp, [cnt + solve(x2, y2, arr[target])] + [x1, y1] + arr[target])
+        q = tmp         
+    return heapq.heappop(q)[0]
