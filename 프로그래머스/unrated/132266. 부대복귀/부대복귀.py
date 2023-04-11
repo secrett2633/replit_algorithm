d3850bfc177dd7d4360483a460c9fcac28eb5c2d
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-04-11 16:08:28 +0900

    [unrated] Title: 부대복귀, Time: 161.27 ms, Memory: 46.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132266. 부대복귀/부대복귀.py
@@ -0,0 +1,17 @@
+def solution(n, roads, sources, destination):
+    answer = []
+    graph = {i : [] for i in range(n + 1)}
+    for a, b in roads:
+        graph[a].append(b)
+        graph[b].append(a)
+    from collections import deque
+    visited = [-1] * (n + 1)
+    q = deque([[0, destination]])
+    while q:
+        cnt, now = q.popleft()
+        if visited[now] != -1: continue
+        visited[now] = cnt
+        for k in graph[now]:
+            q.append([cnt + 1, k])
+    visited[destination] = 0
+    return [visited[i] for i in sources]
