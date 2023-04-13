085225bdd90e145e2a6c4fbac7bc55dc290dd22b
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-04-13 20:56:03 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 3632 ms, Memory: 358972 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,20 +1,16 @@
 import heapq
-
-t = int(input())
-
-for i in range(t):
+import sys
+input = sys.stdin.readline
+for i in range(int(input())):
     k = int(input())
     q1, q2 = [], []
     visited = [False] * k
-
     for j in range(k):
         com, num = input().split()
-
         if com == 'I':
             heapq.heappush(q1, (int(num), j))
             heapq.heappush(q2, (-int(num), j))
             visited[j] = True
-
         else:
             if num == '1':
                 while q2 and not visited[q2[0][1]]:
@@ -33,10 +29,5 @@ for i in range(t):
         heapq.heappop(q1)
     while q2 and not visited[q2[0][1]]:
         heapq.heappop(q2)
-
-    if not q1 or not q2:
-        print("EMPTY")
-    else:
-        a = -q2[0][0]
-        b = q1[0][0]
-        print("%d %d" % (a, b))
+    if not q1 or not q2: print("EMPTY")
+    else: print(-q2[0][0], q1[0][0])
