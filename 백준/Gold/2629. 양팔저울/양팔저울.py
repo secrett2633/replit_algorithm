23e9b22eec3c42aba5c7f2ec0b754f8776c36f17
secrett2633 <65999962+secrett2633@users.noreply.github.com>
2023-05-07 12:54:41 +0900

    [Gold III] Title: 양팔저울, Time: 124 ms, Memory: 114640 KB -BaekjoonHub

---
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+dp = [0] * 40001
+for v in list(map(int, input().split())):
+    for i in range(40000, v, -1):
+        if dp[i - v]:
+            dp[i] = 1
+    dp[v] = 1
+m = int(input())
+arr = list(map(int, input().split()))
+res = []
+for i in arr:
+    if dp[i]: res.append('Y'); continue
+    for j in range(1, 40001 - i):
+        if dp[i + j] and dp[j]:
+            res.append('Y')
+            break
+    else: res.append('N')
+print(' '.join(res))
+        
